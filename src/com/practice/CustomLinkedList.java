6e314cbb3ff739266e253a0437ae0ff9e394fa71
Shubham-Itech2 <shubhamshukla@i-techsoftware.com>
2023-03-14 15:55:07 -0400

    commit for minor changes in feature

--- src/com/practice/CustomLinkedList.java
+++ src/com/practice/CustomLinkedList.java
@@ -38,7 +38,7 @@ public class CustomLinkedList {
         list.insert(5);
         list.insert(6);
         list.print();
-        System.out.println("Hello this is main");
+        System.out.println("Hello this is feature1");
     }
 
 
