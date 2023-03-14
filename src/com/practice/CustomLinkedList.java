5f905461e7176be194b00e8e3746114df8aa729b
Shubham-Itech2 <shubhamshukla@i-techsoftware.com>
2023-03-14 16:07:16 -0400

    conflicts

--- src/com/practice/CustomLinkedList.java
+++ src/com/practice/CustomLinkedList.java
@@ -44,9 +44,9 @@ public class CustomLinkedList {
 
     class Node{
         int data;
-        Node next;
+        int next;
 
-        public Node(int data){
+        private Node(int data){
             this.data = data;
         }
 
